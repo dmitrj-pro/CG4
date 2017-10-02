b97abd77954f69d9f41485335153a6234f8a0ce4
Diman PRO <diman-pro@myprogram.us>
2017-10-02 19:40:21 +0300

    Start write class Matrix

---
+++ CG4/Matrix.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace CG4
+{
+	public class Matrix
+	{
+		private int [][] _matr;
+		private int _m;
+		private int _n;
+		public Matrix (int n, int m)
+		{
+			if (n < 0 || m <0)
+				throw new Exception ("size>0");
+			_m = m;
+			_n = n;
+			_matr = new int[_m][];
+			for (int i = 0; i < _m; i++)
+				_matr [i] = new int[_n];
+		}
+
+		public void Print(){
+			for (int i = 0; i < _m; i++) {
+				for (int j = 0; j < _n; j++) {
+					Console.Write (_matr [i][j]);
+					Console.Write (" ");
+				}
+				Console.WriteLine ();
+
+			}
+				
+		}
+
+		public void Set(int x, int i, int j){
+			if (i > _m || j > _n || i < 0 || j < 0)
+				throw new Exception ();
+			_matr [i] [j] = x;
+		}
+
+		public Matrix Mult(Matrix m2){
+			Matrix res = new Matrix(m2._n, _m);
+
+			for (int i = 0; i < _m; i++) {
+				for (int j = 0; j < m2._n; j++) {
+					for (int k = 0; k < m2._m; k++) {
+						res._matr [i] [j] += _matr [i] [k] * m2._matr [k] [j];
+					}
+				}
+			}
+
+			res.Print ();
+			return res;
+		}
+	}
+}
+
---
+++ CG4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CG4
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Matrix m = new Matrix (2, 3);
+			m.Set (1, 0, 0);
+			m.Set (2, 0, 1);
+			m.Set (3, 1, 0);
+			m.Set (4, 1, 1);
+			m.Set (5, 2, 0);
+			m.Set (6, 2, 1);
+			m.Print ();
+
+			Matrix m1 = new Matrix (2, 2);
+			m1.Set (1, 0, 0);
+			m1.Set (2, 0, 1);
+			m1.Set (2, 1, 0);
+			m1.Set (3, 1, 1);
+			m1.Print ();
+			m.Mult (m1).Print ();
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
